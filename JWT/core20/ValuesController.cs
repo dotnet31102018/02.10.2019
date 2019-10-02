60ef3695d795e9616d28b8c1f5f4634c23963c79
dotnet31102018 <44984976+dotnet31102018@users.noreply.github.com>
2019-10-02 19:43:35 +0300

    Add files via upload

---
+++ JWT/core20/ValuesController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [Authorize(Roles = "Administrator")]
+    public class ValuesController : Controller
+    {
+        [HttpGet("get-my-id")]
+        public ActionResult GetMyId()
+        {
+            var idClaim = User.Claims.FirstOrDefault(x => x.Type.Equals("id", StringComparison.InvariantCultureIgnoreCase));
+            if (idClaim != null)
+            {
+                return Ok($"This is your Id: {idClaim.Value}");
+            }
+            return BadRequest("No claim");
+        }
+
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            var idClaim = User.Claims.FirstOrDefault(x => x.Type.Equals("id", StringComparison.InvariantCultureIgnoreCase));
+            return new string[] { "value1", "value2" , idClaim.Issuer};
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
